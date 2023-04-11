fb155bfab960089181be26340bb5e4178ac129ec
Mohit <nagarmohit138@gmail.com>
2023-04-11 12:05:19 +0530

    String builder and stack

---
+++ code/String/Removing Stars From a String.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String removeStars(String s) {
+      
+
+        Stack<Character>st = new Stack<>();
+
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+
+            if(ch != '*'){
+                st.push(ch);
+            }else{
+                st.pop();
+            }
+        }
+
+        StringBuilder ans = new StringBuilder();
+        while(st.size() != 0){
+            ans.append(st.pop());
+        }
+    
+        return ans.reverse().toString();
+
+       
+    }
+}
