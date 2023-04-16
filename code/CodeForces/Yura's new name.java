f92270e2421b2127405a88b1d0d61cd48235a43c
Mohit <nagarmohit138@gmail.com>
2023-04-16 21:53:45 +0530

    dp

--- code/CodeForces/Yura's new name.java
+++ code/CodeForces/Yura's new name.java
@@ -28,3 +28,118 @@ public class Main{
         }
     }
 }
+
+
+
+// string s;
+// cin >> s;
+
+// int n = s.size();
+
+// int longest =0;
+// int current = 0;
+
+// int count = 0;
+
+// for(int i=0;i<n;i++){
+// 	if(s[i] == '0'){
+// 		++count;
+//         longest = max(longest,current);
+//         current =0;
+//         continue;
+// 	}
+//     ++current;
+
+// }
+
+// if(count == 0){
+//     int ans = n * n;
+//     cout << ans << nl;
+//     return;
+// }
+
+// int extra = 0;
+
+// for(int i=0;i<n;i++){
+//     if(s[i] == '1'){
+//         ++extra;
+//     }else{
+//         break;
+//     }
+// }
+
+// for(int i=n;i>-1;i--){
+//     if(s[i] == '1'){
+//         ++extra;
+//     }else{
+//         break;
+//     }
+// }
+
+// longest = max({longest,current,extra});
+
+// int ans = 0;
+
+// for(int i=1;i<longest+1;i++){
+//     ans = max(ans,i*(longest-i+1));
+// }
+
+// cout<<ans<<nl;
+
+// import java.util.Scanner;
+
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+//         String s = scanner.nextLine();
+//         int n = s.length();
+
+//         int longest = 0;
+//         int current = 0;
+//         int count = 0;
+
+//         for (int i = 0; i < n; i++) {
+//             if (s.charAt(i) == '0') {
+//                 count++;
+//                 longest = Math.max(longest, current);
+//                 current = 0;
+//                 continue;
+//             }
+//             current++;
+//         }
+
+//         if (count == 0) {
+//             int ans = n * n;
+//             System.out.println(ans);
+//             return;
+//         }
+
+//         int extra = 0;
+
+//         for (int i = 0; i < n; i++) {
+//             if (s.charAt(i) == '1') {
+//                 extra++;
+//             } else {
+//                 break;
+//             }
+//         }
+
+//         for (int i = n - 1; i >= 0; i--) {
+//             if (s.charAt(i) == '1') {
+//                 extra++;
+//             } else {
+//                 break;
+//             }
+//         }
+
+//         longest = Math.max(Math.max(longest, current), extra);
+
+//         int ans = 0;
+
+//         for (int i = 1; i < longest + 1; i++) {
+//             ans = Math.max(ans, i * (longest - i + 1));
+//         }
+
+//         System.out.println(ans);
+//     }
+// }
