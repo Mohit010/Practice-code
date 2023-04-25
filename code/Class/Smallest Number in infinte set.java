b6d93fc853005a562657474c5bf3f906a3ef43e5
Mohit <nagarmohit138@gmail.com>
2023-04-25 10:55:52 +0530

    classes and objects

---
+++ code/Class/Smallest Number in infinte set.java
@@ -0,0 +1,26 @@
+class SmallestIntfinteSet{
+    HashSet<Integer>hs;
+    int curr;
+
+    public SmallestIntfinteSet(){
+        hs = new HashSet<>();
+        curr = 1;
+    }
+
+    public int popSmallest(){
+        if(hs.size() != 0){
+            int res = Collections.min(hs);
+            hs.remove(res);
+            return res;
+        }else{
+            curr++;
+            return curr-1;
+        }
+    }
+
+    public void addBack(int num){
+        if(num < curr){
+            hs.add(num);
+        }
+    }
+}
