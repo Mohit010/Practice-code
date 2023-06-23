c6ebfbc94eb74c5b453f7bfa8653f3fe276ed0d5
Mohit <nagarmohit138@gmail.com>
2023-06-23 19:33:50 +0530

    cf alice and bob

---
+++ code/CodeForces/Alice and bob.java
@@ -0,0 +1,18 @@
+public import java.util.*;
+
+public class Main{
+    public static void main(String args[]){
+        Scanner scn = new Scanner(System.in);
+        int t = scn.nextInt();
+        while(t-- > 0){
+            int n = scn.nextInt();
+            if(n >= 5){
+                System.out.println("Alice");
+            }else{
+                System.out.println("Bob");
+            }
+        }
+    }
+} Alice and bob {
+    
+}
